ebbb859c603e336ce914e8fea4ce788dd6217b22
Monmerque <a.a@a.com>
2019-10-31 17:08:22 +0100

    Try all with _blank

---
+++ precache-manifest.8357a344205670064d98d8aeb78fe75c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95269d348bcc323ce4926670e1ba85e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9abd7fdba864e6d91cda",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2fd27ffd20aefc1f43b0",
+    "url": "/static/js/2.a17f861f.chunk.js"
+  },
+  {
+    "revision": "9abd7fdba864e6d91cda",
+    "url": "/static/js/main.e9857d31.chunk.js"
+  },
+  {
+    "revision": "34dc5293102aed708ca3",
+    "url": "/static/js/runtime-main.a189f95f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.540b310afa5d1e1b8cafc83481eae3f0.js"
+  "/precache-manifest.8357a344205670064d98d8aeb78fe75c.js"
 );
 
 self.addEventListener('message', (event) => {
