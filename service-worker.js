177b0ea08123e7e179e16c87d0034fc0ba64c376
Monmerque <a.a@a.com>
2019-10-31 16:32:51 +0100

    Test all features

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.29bbe6ac8fece7383ed36a942c4a0f92.js"
+  "/precache-manifest.2fb3e89719cdcfe221b87a55fd04055e.js"
 );
 
 self.addEventListener('message', (event) => {
